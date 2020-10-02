2b1ccafcdbbf6099b0fed1bad9b13903f08247df
und3f1ned <dev@xedin.ru>
2020-10-02 21:01:24 +0300

    Added MaxCircuitDirtiness to torrc

--- services/tor.service.js
+++ services/tor.service.js
@@ -8,22 +8,25 @@ const writeTorConfig = async (startPort, count) => {
   logger.info('App running in production. Will use rotating proxy via TOR.');
   logger.info(' Writing Tor Config');
   await execWithPromise('touch /etc/tor/torrc && echo > /etc/tor/torrc');
+  await execWithPromise('echo "MaxCircuitDirtiness 10" >> /etc/tor/torrc');
   const promiseArr = [];
   for (let i = 0; i < count; i += 1) {
     const port = startPort + i;
     promiseArr.push(
-      execWithPromise(
-        `echo "SocksPort ${port}" >> /etc/tor/torrc`,
-      ).then(() => logger.debug(`PORT ${port} written in tor config`)),
+      execWithPromise(`echo "SocksPort ${port}" >> /etc/tor/torrc`).then(() =>
+        logger.debug(`PORT ${port} written in tor config`)
+      )
     );
   }
-  return Promise.all(promiseArr).then(() => {
-    logger.success('Tor Config written successfully.');
-  }).catch((error) => {
-    logger.error('One or more ports couldn\'t be written into tor config.');
-    logger.debug(error);
-    throw new Error();
-  });
+  return Promise.all(promiseArr)
+    .then(() => {
+      logger.success('Tor Config written successfully.');
+    })
+    .catch((error) => {
+      logger.error("One or more ports couldn't be written into tor config.");
+      logger.debug(error);
+      throw new Error();
+    });
 };
 
 const stopTor = async () => {
@@ -31,7 +34,9 @@ const stopTor = async () => {
   try {
     await execWithPromise('pkill -9 -f "tor"');
   } catch (error) {
-    logger.warn('Failed to stop TOR. Usually this is a no op but ensure the subsequent attempts are using different IPs.');
+    logger.warn(
+      'Failed to stop TOR. Usually this is a no op but ensure the subsequent attempts are using different IPs.'
+    );
     logger.debug(error);
   }
 };
